3924678685684b8d634f1ffdf266e50173f8308a
mazureth <jesse.mazur@joon.com>
2024-03-15 15:46:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/components/Crossmint.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useState } from "react";
+import { CrossmintPaymentElement } from "@crossmint/client-sdk-react-ui";
+import Minting from "./Minting";
+
+const Crossmint: React.FC = () => {
+  const [orderIdentifier, setOrderIdentifier] = useState<string | null>(null);
+
+  const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string;
+  const collectionId = process.env.NEXT_PUBLIC_COLLECTION_ID as string;
+  const environment = process.env.NEXT_PUBLIC_ENVIRONMENT as string;
+
+  return (
+    <>
+      <div className="sm:col-span-3">
+        {orderIdentifier === null ? (
+          <CrossmintPaymentElement
+            projectId={projectId}
+            collectionId={collectionId}
+            environment={environment}
+            emailInputOptions={{
+              show: true,
+            }}
+            mintConfig={{
+              type: "erc-721",
+              totalPrice: "0.001",
+            }}
+            onEvent={(event) => {
+              switch (event.type) {
+                case "payment:process.succeeded":
+                  console.log(event);
+                  setOrderIdentifier(event.payload.orderIdentifier);
+                  break;
+                default:
+                  console.log(event);
+                  break;
+              }
+            }}
+          />
+        ) : (
+          <Minting orderIdentifier={orderIdentifier} />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Crossmint;
